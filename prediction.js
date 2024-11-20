1b87c087875f7c8f29d762e4116962256b666967
JenniferLe03 <jennifer.le1@edu.sait.ca>
2024-11-20 15:57:56 -0700

    added function in js to shake

---
+++ prediction.js
@@ -0,0 +1,6 @@
+shake.addEventListener("click", function() {
+    console.log("Shake it off!");
+});
+
+/* we don't want js to run soemthing until something happens*/
+/*telling it to run everything in this function*/
