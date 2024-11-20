b5a0181a1fb2585d1b620a25d39b9c13949fe556
JenniferLe03 <jennifer.le1@edu.sait.ca>
2024-11-20 16:31:54 -0700

    let variables

--- prediction.js
+++ prediction.js
@@ -1,6 +1,28 @@
-shake.addEventListener("click", function() {
-    console.log("Shake it off!");
+shake.addEventListener('click', function () {
+	console.log('Shake it off!');
+
+	let response = Math.random();
+	console.log(response);
+    response = Math.floor(Math.random() * 11); //0-10
+
+    if (response==0){
+        output.innerHTML = "Yes, indeed"
+    }
+
+    if (response ==1){
+        output.innerHTML = "Ask again later"
+    }
+
+    if (response ==2){
+        output.innerHTML = "Very doubtful"
+    }
+
+    console.log(response);
 });
+
+//response - Math.floor(Math.random()*9)+1; this code is a way to generate it from 0-10
 
 /* we don't want js to run soemthing until something happens*/
 /*telling it to run everything in this function*/
+/*"let" variable let you change something often"*/
+
